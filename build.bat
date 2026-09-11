a861549f3be351efaa3050bb8536ba91dcc033a4
an04wallt-spec <an04wallt@gmail.com>
2026-09-11 14:32:31 +0300

    Apply leader fix in Windows build

--- build.bat
+++ build.bat
@@ -16,6 +16,8 @@ python patch_frame_params.py
 if errorlevel 1 exit /b 1
 python patch_syntax_fix.py
 if errorlevel 1 exit /b 1
+python patch_leader_fix.py
+if errorlevel 1 exit /b 1
 python patch_native.py
 if errorlevel 1 exit /b 1
 if not exist build mkdir build
